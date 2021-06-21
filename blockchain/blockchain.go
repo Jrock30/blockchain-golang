df8a74cc4c594c65ba73ee6a9eaf652f71747feb
Jrock30 <jrg8989@knou.ac.kr>
2021-06-22 00:14:14 +0900

    refactor: blockchain package - make blockchain package - Singleton Pattern add

--- blockchain/blockchain.go
+++ blockchain/blockchain.go
@@ -1,1 +1,69 @@
 package blockchain
+
+import (
+	"crypto/sha256"
+	"fmt"
+	"sync"
+)
+
+type block struct {
+	Data     string
+	Hash     string
+	PrevHash string // 이전의 hash
+
+	// one-way function (단 방향으로만 실행할 수 있다, 결정론적이다.(입력값은 항상 같은 출력값을 얻게 된다.))
+}
+
+type blockchain struct {
+	blocks []*block // block 의 slice 가 아닌 pointer 들의 slice (복사 X)
+}
+
+var b *blockchain // SingleTon
+var one sync.Once // sync package
+
+/* receiver function (Method)
+Calculate Hash
+*/
+func (b *block) calculateHash() {
+	hash := sha256.Sum256([]byte(b.Data + b.PrevHash)) // sha256 Byte Code Hash 생성 (data + prevHash)
+	b.Hash = fmt.Sprintf("%x", hash)                   // 바이트 코드를 16진수 String 으로 변환
+}
+
+// Get Last Block Hash
+func getLastHash() string {
+	totalBlocks := len(GetBlockchain().blocks)
+	if totalBlocks == 0 {
+		return ""
+	}
+	return GetBlockchain().blocks[totalBlocks-1].Hash
+}
+
+// Create New Block
+func createBlock(data string) *block {
+	newBlock := block{data, "", getLastHash()}
+	newBlock.calculateHash()
+	return &newBlock
+}
+
+// export function
+func (b *blockchain) AddBlock(data string) {
+	b.blocks = append(b.blocks, createBlock(data))
+}
+
+// GetBlockchain SingleTon Pattern (소문자를 사용해서 밖에서 접근할 수 없도록 한다)
+func GetBlockchain() *blockchain {
+	if b == nil {
+		// go routine, thread 몇 개든 병렬적으로 실행해도 한번만 실행된다.
+		one.Do(func() {
+			b = &blockchain{}
+			b.AddBlock("Genesis Block")
+		})
+	}
+	return b
+}
+
+func (b *blockchain) AllBLocks() []*block {
+	//func AllBlocks() []*block {
+	//	return GetBlockchain().blocks
+	return b.blocks
+}
--- main.go
+++ main.go
@@ -1,60 +1,21 @@
 package main
 
 import (
-	"crypto/sha256"
 	"fmt"
+	"github.com/jrock30/coin-basic/blockchain"
 )
-
-type block struct {
-	data     string
-	hash     string
-	prevHash string // 이전의 hash
-
-	// one-way function (단 방향으로만 실행할 수 있다, 결정론적이다.(입력값은 항상 같은 출력값을 얻게 된다.))
-}
-
-type blockchain struct {
-	blocks []block
-}
-
-// Get Last Block Hash
-func (b *blockchain) getLastHash() string {
-	// 블록이 기존에 하나라도 존재하면 마지막 블록 Hash 값 가져 온다.
-	if len(b.blocks) > 0 {
-		return b.blocks[len(b.blocks)-1].hash
-	}
-	return ""
-}
-
-// receiver function
-func (b *blockchain) addBlock(data string) {
-	// block struct 생성
-	newBlock := block{data, "", b.getLastHash()}
-	// sha256 Byte Code Hash 생성
-	hash := sha256.Sum256([]byte(newBlock.data + newBlock.prevHash))
-	// 바이트 코드를 16진수 String 으로 변환
-	newBlock.hash = fmt.Sprintf("%x", hash)
-	// 새로운 블록을 기존 블록에 추가
-	b.blocks = append(b.blocks, newBlock)
-}
-
-// receiver function
-func (b *blockchain) listBlocks() {
-	// index 무시
-	for _, block := range b.blocks {
-		fmt.Printf("Data: %s\n", block.data)
-		fmt.Printf("Hash: %s\n", block.hash)
-		fmt.Printf("Prev Hash: %s\n\n", block.prevHash)
-	}
-}
 
 /*
   Main Package (Entry Point)
 */
 func main() {
-	chain := blockchain{}
-	chain.addBlock("Genesis Block")
-	chain.addBlock("Second Block")
-	chain.addBlock("Third Block")
-	chain.listBlocks()
+	chain := blockchain.GetBlockchain()
+	chain.AddBlock("Second Block")
+	chain.AddBlock("Third Block")
+	chain.AddBlock("Fourth Block")
+	for _, block := range chain.AllBLocks() {
+		fmt.Printf("Data: %s\n", block.Data)
+		fmt.Printf("Hash: %s\n", block.Hash)
+		fmt.Printf("PrevHash: %s\n", block.PrevHash)
+	}
 }
